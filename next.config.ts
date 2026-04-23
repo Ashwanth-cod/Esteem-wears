82f7fe5af0eee87de9eecf78169455715e7fa6d2
Ashwanth s <FI.programming.tamil@gmail.com>
2026-04-23 15:57:41 +0530

    sample

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.29.101", "esteemwears.in"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "drive.google.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
