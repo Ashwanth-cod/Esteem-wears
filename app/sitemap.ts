f50a9740d3a227fa12017156909e22b62ebf6e66
Ashwanth s <FI.programming.tamil@gmail.com>
2026-04-28 20:45:18 +0530

    Sitemap created

---
+++ app/sitemap.ts
@@ -0,0 +1,28 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://esteemwears.in/",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://esteemwears.in/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://esteemwears.in/catalogue",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://esteemwears.in/catalogue/boys",
+      lastModified: new Date(),
+    },    
+    {
+      url: "https://esteemwears.in/catalogue/girls",
+      lastModified: new Date(),
+    },    
+    {
+      url: "https://esteemwears.in/catalogue/men",
+      lastModified: new Date(),
+    }
+  ];
+}
